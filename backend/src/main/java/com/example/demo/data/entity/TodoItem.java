ecde1ce0aa2878cfce27de1d2ae6f780ba03917c
Omer <soner.cxtdemir@gmail.com>
2022-10-19 11:20:38 -0400

    angular with java

--- backend/src/main/java/com/example/demo/data/entity/TodoItem.java
+++ backend/src/main/java/com/example/demo/data/entity/TodoItem.java
@@ -10,7 +10,6 @@ public class TodoItem {
     @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
     private String todo;
-
     private boolean checking;
 
     public TodoItem (){
